4ed1803a0c841d8a3bdab3d2c4a65ea3784e6992
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-08-14 11:11:09 +0100

    add word-play2

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $(".words").submit(function(event){
+
+        event.preventDefault();
+    
+        let wordInput = $("#word").val();
+        // console.log(wordInput)
+
+
+        let emptyarr= []
+        // let sentence = [wordInputnput] 
+        // console.log(sentence)
+
+        let splitArr = wordInput.split([""]);
+        // console.log(splitArr);
+
+        splitArr.forEach( function(sentence) {
+            emptyarr.push(sentence)
+            // if (sentence.length >= 3 ) {
+            //    emptyarr.push(sentence);
+
+            // }
+
+
+             console.log(emptyarr);
+
+          
+            
+        });
+
+        // splitArray.forEach(function(word) {
+            
+        //     if ( word.length >=  3 ) {
+        //     emptyarr.push(word);
+        // }
+            
+        // });
+
+        // console.log(emptyarr);
+
+        emptyarr = emptyarr.reverse();
+        emptyarr = emptyarr.join("");
+
+
+        console.log(emptyarr);
+
+        
+       
+
+        
+    
+
+    });
+
+});
